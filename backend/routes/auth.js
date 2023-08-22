f1a883b433de8571fb9186caef38e8440f14715f
Shahanawaz <shahanawazkhan579@gmail.com>
2023-08-22 11:51:17 +0530

    Added JSON web token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "thisismysecretjwt";
 
 // Create a User using: POST "/api/auth/createuser". No login required
 router.post(
@@ -20,22 +24,32 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // Check whether the user with this email exists already
     try {
+      // Check whether the user with this email exists already
       let user = await User.findOne({ email: req.body.email });
       if (user) {
         return res
           .status(400)
           .json({ error: "Sorry a user with this email already exists" });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       // Create a User
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
 
-      res.json(user);
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send(error.message);
