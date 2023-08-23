f426eede2147eae4a44c886b2a8515e04bcc550e
Shahanawaz <shahanawazkhan579@gmail.com>
2023-08-23 15:54:05 +0530

    Created components & Provided routes to them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcom to iNotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
