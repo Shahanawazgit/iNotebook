c292875eb025a0760079bd01b6c6c45ae367840d
Shahanawaz <shahanawazkhan579@gmail.com>
2023-08-24 14:31:08 +0530

    Added deleteNote functionality

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
-import Noteitem from "./Noteitem";
+import NoteItem from "./NoteItem";
 import AddNote from "./AddNote";
 
 const Notes = () => {
   const context = useContext(noteContext);
-  const { notes, addNote } = context;
+  const { notes } = context;
 
   return (
     <>
@@ -13,7 +13,7 @@ const Notes = () => {
       <div className="row my-3">
         <h2>Your Notes</h2>
         {notes.map((note) => {
-          return <Noteitem key={note._id} note={note} />;
+          return <NoteItem key={note._id} note={note} />;
         })}
       </div>
     </>
