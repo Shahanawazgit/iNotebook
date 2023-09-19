5dcedc27aad762162b25115607684649710535b0
Shahanawaz <shahanawazkhan579@gmail.com>
2023-09-19 16:53:20 +0530

    removed about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import About from "./components/About";
 import NoteState from "./context/notes/NoteState";
 import Alert from "./components/Alert";
 import Login from "./components/Login";
@@ -30,17 +29,8 @@ function App() {
           <div className="container">
             <Routes>
               <Route exact path="/" element={<Home showAlert={showAlert} />} />
-              <Route exact path="/about" element={<About />} />
-              <Route
-                exact
-                path="/login"
-                element={<Login showAlert={showAlert} />}
-              />
-              <Route
-                exact
-                path="/signup"
-                element={<Signup showAlert={showAlert} />}
-              />
+              <Route exact path="/login" element={<Login showAlert={showAlert} />} />
+              <Route exact path="/signup" element={<Signup showAlert={showAlert} />} />
             </Routes>
           </div>
         </Router>
