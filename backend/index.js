c04e739cb8b0199bc964209b7447d7d36f6f04ab
Shahanawaz <shahanawazkhan579@gmail.com>
2023-09-20 15:21:10 +0530

    updated index.js

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
 
-// app.get("/", (req, res) => {
-//   res.send("Hello Shan!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello Shan!");
+});
 
 app.listen(port, () => {
   console.log(`iNotebook backend live at ${port}`);
