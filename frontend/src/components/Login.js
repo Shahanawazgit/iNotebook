ad5e4f3351a7cdc02a19deea8579b2a3eaebfc97
Shahanawaz <139430501+Shahanawazgit@users.noreply.github.com>
2023-09-20 16:36:00 +0530

    Added test account

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -37,49 +37,57 @@ const Login = (props) => {
   const mobile = window.innerWidth <= 768 ? true : false;
 
   return (
-    <div className="container my-2">
-      <form onSubmit={handleSubmit}>
-        <h2>Login to Your Account</h2>
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            name="email"
-            aria-describedby="emailHelp"
-            onChange={onChange}
-            autoComplete="off"
-            style={{ width: mobile ? "100%" : "50%" }}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            className="form-control"
-            id="password"
-            name="password"
-            onChange={onChange}
-            autoComplete="off"
-            style={{ width: mobile ? "100%" : "50%" }}
-          />
-        </div>
-        <button type="submit" className="btn btn-success">
-          Login
-        </button>
-        <p className="mt-2">
-          Don't have an Account?{" "}
-          <Link to="/signup">
-            <button className="btn btn-primary">Signup Now</button>
-          </Link>
-        </p>
-      </form>
-    </div>
+    <>
+      <div className="container my-2">
+        <form onSubmit={handleSubmit}>
+          <h2>Login to Your Account</h2>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              aria-describedby="emailHelp"
+              onChange={onChange}
+              autoComplete="off"
+              style={{ width: mobile ? "100%" : "50%" }}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              onChange={onChange}
+              autoComplete="off"
+              style={{ width: mobile ? "100%" : "50%" }}
+            />
+          </div>
+          <button type="submit" className="btn btn-success">
+            Login
+          </button>
+          <p className="mt-2">
+            Don't have an Account?{" "}
+            <Link to="/signup">
+              <button className="btn btn-primary">Signup Now</button>
+            </Link>
+          </p>
+        </form>
+      </div>
+
+      <div className="container mt-5 text-light bg-warning px-3 rounded-3 bg-opacity-25">
+        <h3 className="pt-2">You can test all the features through this test account:</h3>
+        <p>Email: test@example.com</p>
+        <p className="pb-2">Password: test@123</p>
+      </div>
+    </>
   );
 };
 
