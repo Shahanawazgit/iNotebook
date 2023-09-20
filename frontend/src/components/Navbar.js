6f48ef4257d80e6e59a321d9b0d0e79b0a25bba4
Shahanawaz <139430501+Shahanawazgit@users.noreply.github.com>
2023-09-20 17:14:59 +0530

    removed unneccessory home link

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -18,24 +18,7 @@ const Navbar = () => {
           <Link className="navbar-brand" to="/">
             iNotebook
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} to="/">
-                  Home
-                </Link>
-              </li>
-            </ul>
+          
             {!localStorage.getItem("token") ? (
               <form className="d-flex">
                 <Link className="btn btn-dark mx-1" to={location.pathname === "/login" ? "/signup" : "/login"} role="button">
@@ -47,7 +30,6 @@ const Navbar = () => {
                 Logout
               </button>
             )}
-          </div>
         </div>
       </nav>
     </>
